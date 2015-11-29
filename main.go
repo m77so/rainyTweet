38d0a2a727972b4ae22b45bb3cfc368a316f2045
nakei <m77@m77.so>
2015-11-29 23:24:40 +0900

    go fmt

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
-import(
-	"fmt"
-	"net/http"
-	"time"
-	"bytes"
+import (
 	"archive/tar"
+	"bytes"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"math"
-	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 
 	"image"
-	"image/png"
 	"image/color"
+	"image/png"
 )
 
-const imageWidth  = 2560
+const imageWidth = 2560
 const imageHeight = 3360
 
 const (
-	 UNKNOWN  = iota
-	 NO_RAIN
-	 SPRINKLE
-	 RAIN
-	 DOWNPOUR
+	UNKNOWN = iota
+	NO_RAIN
+	SPRINKLE
+	RAIN
+	DOWNPOUR
 )
 const SPRINKLE_MAX = 25
 const DOWNPOUR_MIN = 53
 
 type rainfallData struct {
-	data []byte
+	data       []byte
 	created_at time.Time
 }
 type weatherBoolData struct {
-	data []bool
+	data       []bool
 	created_at time.Time
-
 }
-func downloadData(t time.Time) ( []byte){
+
+func downloadData(t time.Time) []byte {
 	var buffer bytes.Buffer
 	var url string
 	var file_name string
 	t = t.UTC()
-	t = t.Add(time.Duration(-(t.Minute() % 10)) * time.Minute)     //10分おき に修正
+	t = t.Add(time.Duration(-(t.Minute() % 10)) * time.Minute) //10分おき に修正
 
 	//Download
 	buffer.WriteString("http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/jma-radar/synthetic/original/")
@@ -89,17 +89,17 @@ func downloadData(t time.Time) ( []byte){
 	}
 	return contents
 }
-func decompress(compData *[]byte, maxV byte, res_size int)([]byte){
-	var data []byte = make([]byte,res_size)
+func decompress(compData *[]byte, maxV byte, res_size int) []byte {
+	var data []byte = make([]byte, res_size)
 
 	var p uint32 = 0
 	var runLength uint32 = 0
 	var runLength_digit uint8 = 0
 	var LNGU byte = 255 - maxV
-	for i := 0; i<len(*compData); i++{
+	for i := 0; i < len(*compData); i++ {
 		val := (*compData)[i]
 		//normal value
-		if val <= maxV{
+		if val <= maxV {
 			runLength_digit = 0
 			data[p] = val
 			p++
@@ -107,14 +107,14 @@ func decompress(compData *[]byte, maxV byte, res_size int)([]byte){
 		}
 		//run length value
 		val -= maxV + 1
-		if runLength_digit == 0{
+		if runLength_digit == 0 {
 			runLength = uint32(val)
 		}
-		runLength = uint32(val) * uint32( math.Pow(float64(LNGU),float64(runLength_digit)) )
+		runLength = uint32(val) * uint32(math.Pow(float64(LNGU), float64(runLength_digit)))
 		val = data[p-1]
 
 		lim := runLength + p
-		for j:=p; j<lim; j++{
+		for j := p; j < lim; j++ {
 			data[j] = val
 		}
 		p = lim
@@ -122,61 +122,61 @@ func decompress(compData *[]byte, maxV byte, res_size int)([]byte){
 	}
 	return data
 }
-func getData(t time.Time) (rainfallData) {
+func getData(t time.Time) rainfallData {
 
 	res := downloadData(t)
 	//Open
 	var maxV byte = res[204]
 	//var size uint32 = binary.BigEndian.Uint32(res[716:720])
-	compData := res[721:(len(res)-4)]
+	compData := res[721:(len(res) - 4)]
 
-	data := rainfallData{  data : decompress(&compData,maxV,imageWidth * imageHeight)      }
+	data := rainfallData{data: decompress(&compData, maxV, imageWidth*imageHeight)}
 
 	return data
 }
-func (rd rainfallData)  createPng( filename string){
+func (rd rainfallData) createPng(filename string) {
 
-	f, err := os.OpenFile(filename, os.O_CREATE | os.O_WRONLY, 0666)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	m := image.NewGray(image.Rect(0,0,imageWidth,imageHeight))
-	for y:=0; y<imageHeight; y++{
-		for x:=0; x< imageWidth; x++{
-			c := color.Gray{uint8(255-rd.data[y*imageWidth+x])}
-			m.SetGray(x,y,c)
+	m := image.NewGray(image.Rect(0, 0, imageWidth, imageHeight))
+	for y := 0; y < imageHeight; y++ {
+		for x := 0; x < imageWidth; x++ {
+			c := color.Gray{uint8(255 - rd.data[y*imageWidth+x])}
+			m.SetGray(x, y, c)
 		}
 	}
-	if err = png.Encode(f,m); err != nil {
+	if err = png.Encode(f, m); err != nil {
 		log.Fatalln(err)
 	}
 }
-func (bd weatherBoolData) createPng(filename string){
+func (bd weatherBoolData) createPng(filename string) {
 
-	f, err := os.OpenFile(filename, os.O_CREATE | os.O_WRONLY, 0666)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	m := image.NewGray(image.Rect(0,0,imageWidth,imageHeight))
-	for y:=0; y<imageHeight; y++{
-		for x:=0; x< imageWidth; x++{
+	m := image.NewGray(image.Rect(0, 0, imageWidth, imageHeight))
+	for y := 0; y < imageHeight; y++ {
+		for x := 0; x < imageWidth; x++ {
 
 			c := color.Gray{255}
-			if bd.data[y*imageWidth+x] == true{
+			if bd.data[y*imageWidth+x] == true {
 				c = color.Gray{0}
 			}
-			m.SetGray(x,y,c)
+			m.SetGray(x, y, c)
 		}
 	}
-	if err = png.Encode(f,m); err != nil {
+	if err = png.Encode(f, m); err != nil {
 		log.Fatalln(err)
 	}
 }
-func (rd rainfallData) dump(filename string){
-   ioutil.WriteFile(filename,rd.data,os.ModePerm)
+func (rd rainfallData) dump(filename string) {
+	ioutil.WriteFile(filename, rd.data, os.ModePerm)
 }
-func (rd rainfallData) filterMatch(weather int) (weatherBoolData){
-	var data []bool = make([]bool,len(rd.data))
+func (rd rainfallData) filterMatch(weather int) weatherBoolData {
+	var data []bool = make([]bool, len(rd.data))
 	for i, val := range rd.data {
 		data[i] = false
 		switch weather {
@@ -185,19 +185,19 @@ func (rd rainfallData) filterMatch(weather int) (weatherBoolData){
 				data[i] = true
 			}
 		case NO_RAIN:
-			if val == 1{
+			if val == 1 {
 				data[i] = true
 			}
 		case SPRINKLE:
-			if val >=2 && val < SPRINKLE_MAX{
+			if val >= 2 && val < SPRINKLE_MAX {
 				data[i] = true
 			}
 		case RAIN:
-			if val >= 2{
+			if val >= 2 {
 				data[i] = true
 			}
 		case DOWNPOUR:
-			if val>DOWNPOUR_MIN {
+			if val > DOWNPOUR_MIN {
 				data[i] = true
 			}
 		}
@@ -206,7 +206,7 @@ func (rd rainfallData) filterMatch(weather int) (weatherBoolData){
 }
 
 func main() {
-	data :=getData(time.Now().Add(time.Duration(-60)*time.Minute))
+	data := getData(time.Now().Add(time.Duration(-60) * time.Minute))
 	data.createPng("tmp/aaa.png")
 	data.filterMatch(RAIN).createPng("tmp/rain.png")
 
